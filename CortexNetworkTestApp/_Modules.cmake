d87850100c7f9b0662b6c95d64e97b56025714d3
RVeips <rihardsv@na.lv>
2021-08-12 16:57:33 +0300

    license stuff + refac + cfxs module netif stuff
    Signed-off-by: RVeips <rihardsv@na.lv>

--- CortexNetworkTestApp/_Modules.cmake
+++ CortexNetworkTestApp/_Modules.cmake
@@ -1,19 +1,21 @@
 # Segger RTT printf implementation
 add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/vendor/SeggerRTT")
-target_link_libraries(${EXE_NAME} PRIVATE printf_impl_SeggerRTT)
+target_link_libraries(${EXE_NAME} PUBLIC printf_impl_SeggerRTT)
 
 # driverlib
 add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/vendor/tm4c-driverlib")
-target_link_libraries(${EXE_NAME} PRIVATE TM4C_driverlib)
+target_link_libraries(${EXE_NAME} PUBLIC TM4C_driverlib)
 
 # lwip
 add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/vendor/lwip")
-target_link_libraries(${EXE_NAME} PRIVATE lwip)
+target_link_libraries(${EXE_NAME} PUBLIC lwip)
 
 # CFXS Base
 add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/vendor/CFXS-Base")
-target_link_libraries(${EXE_NAME} PRIVATE CFXS_Base)
+target_link_libraries(${EXE_NAME} PUBLIC CFXS_Base)
 
 # CFXS IP
 add_subdirectory("${CMAKE_CURRENT_SOURCE_DIR}/vendor/CFXS-IP")
-target_link_libraries(${EXE_NAME} PRIVATE CFXS_IP)
+target_include_directories(CFXS_IP PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}/vendor/CFXS-Base/include")
+target_compile_definitions(CFXS_IP PUBLIC "CFXS_IP_IPv4")
+target_link_libraries(${EXE_NAME} PUBLIC CFXS_IP)
--- CortexNetworkTestApp/src/main.cpp
+++ CortexNetworkTestApp/src/main.cpp
@@ -1,5 +1,6 @@
+// ---------------------------------------------------------------------
 // CFXS ARM Network Test App <https://github.com/RVeips/ARM-Cortex-Network-Test>
-// Copyright (C) 2021 - CFXS / Rihards Veips
+// Copyright (C) 2021 | CFXS / Rihards Veips
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -8,11 +9,12 @@
 // 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
+// ---------------------------------------------------------------------
 // [CFXS] //
 
 #include "lwiplib.h"
